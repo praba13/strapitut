a0ed3ecabe6883ef737536b4ed020a30119bf991
Praba <prama1303@gmail.com>
2022-12-30 00:02:39 +0100

    react-router-dom works fine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,44 @@
+import { createBrowserRouter, RouterProvider, Outlet, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Category from './pages/Category';
+import ReviewDetails from './pages/ReviewDetails';
+
+import Header from './components/Header';
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/details/:id',
+        element: <ReviewDetails />
+      },
+      {
+        path: '/category/:id',
+        element: <Category />
+      }],
+  },
+]);
+
 function App() {
   return (
-    <div className="App">
-      <h1>Let's Start with Strapi</h1>
+    <div className='App'>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="site-header">
+            <h1>PR-Reviews</h1>
+        </div>
+    );
+};
+
+export default Header;
