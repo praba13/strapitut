e150420dca6821dacfd9d88961b71283f8142f06
Praba <prama1303@gmail.com>
2023-01-05 15:59:19 +0100

    fetch data from strapi api

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
 
 const Home = () => {
+    const { loading, error, data } = useFetch('http://localhost:1337/api/reviews');
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error...</p>;
+    console.log(data);
     return (
         <div>
-            <h1>Home</h1>
+            {data.data.map((review) => (
+                <div key={review.attributes.id} className="review-card">
+                    <div className="rating">{review.attributes.rating}</div>
+                    <h2>{review.attributes.title}</h2>
+                    <small>console list</small>
+                    <p>{review.attributes.body.substring(0, 200)}...</p>
+                    <Link to={`/details/${review.attributes.id}`}>Read more</Link>
+                </div>
+            ))}
         </div>
     );
+
 };
 
 export default Home;
